2a94b40002ef1d1443c132d7350ca7d130e044c0
NanoTrasen-Inc <nano@nanotrasen.org>
2023-10-07 17:00:53 -0500

    Convert from yarn to npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  purge: {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
-    options: {
-      safelist: {
-        standard: [],
-      },
-    },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
+  safelist: {
+    standard: [],
   },
-  darkMode: false, // or 'media' or 'class'
   theme: {},
-  variants: {
-    extend: {
-      "border-b": ["hover"],
-    },
-  },
   plugins: [],
 };
-
